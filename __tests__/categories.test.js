b498783caf999a7a0e935383b96a488fa7d6e0c7
Benjamin Clark <benisin@gmail.com>
2019-08-15 12:55:00 -0700

    made categories.test.js test for delete()

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -12,7 +12,9 @@ describe('Categories Model', () => {
   it('sanitize() returns undefined with missing requirements', () => {
     const schema = categories.schema;
     var testRecord = {};
+
     for (var field in schema) {
+      // if this field is required create it with null value
       if (schema[field].required) {
         testRecord[field] = null;
       }
@@ -29,6 +31,23 @@ describe('Categories Model', () => {
         });
       })
       .catch(e => console.error('ERR', e));
+  });
+
+  it('can delete() a category', () => {
+    //create 3 categories
+    let obj1 = { name: 'Test Category1' };
+    let obj2 = { name: 'Test Category2' };
+    let obj3 = { name: 'Test Category3' };
+    categories.create(obj1);
+    categories.create(obj2);
+    categories.create(obj3);
+
+    // delete 1 categorey
+    return categories.delete(categories.database[1].id)
+      .then(() => {
+        expect(categories.database.length).toEqual(2);
+      })
+
   });
 
   it('can get() a category', () => {
